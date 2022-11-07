004443cb7b3881a4f8cc9e190785138ba867397a
James Bellenger <james@bellenger.org>
2022-11-07 11:19:41 -0800

    linux updates

--- jmb-bundle.sh
+++ jmb-bundle.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 OUTPUT=`pwd`/build/jmb_flip_fluids_addon.zip
+SKIP_BUILD=${SKIP_BUILD:-0}
 
 set -ex
 
-python3.9 ./build.py
+if [[ $SKIP_BUILD -ne 1 ]]; then
+  python3.10 ./build.py
+fi
 
 cd build/bl_flip_fluids
 
 rm -f $OUTPUT
 zip -r $OUTPUT .
-if [ -e ~/Desktop/blender/addons ]; then
-  cp -v $OUTPUT ~/Desktop/blender/addons
+if [ -e /tank/blender/addons ]; then
+  cp -v $OUTPUT /tank/blender/addons
 fi
